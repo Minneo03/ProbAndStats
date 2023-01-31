92be2be0566d2141e428bba0bba5696c7e734f3f
RyanMinneo <minneor@go.stockton.edu>
2023-01-31 00:14:56 -0500

    Added findIntersection Method
    Don't know if it works yet, but intersection seems simple enough.

--- ProbAndStat/src/Library/SetOperations.java
+++ ProbAndStat/src/Library/SetOperations.java
@@ -7,6 +7,13 @@ import java.util.ArrayList;
  */
 public class SetOperations {
 	
+	/**
+	 * 
+	 * 
+	 * @param set1
+	 * @param set2
+	 * @return
+	 */
 	public ArrayList<Integer> findUnion(ArrayList<Integer> set1, ArrayList<Integer> set2)
 	{
 		ArrayList<Integer> union = new ArrayList<Integer>();
@@ -14,5 +21,28 @@ public class SetOperations {
 		union.addAll(set1);
 		
 		return union;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param set1
+	 * @param set2
+	 * @return
+	 */
+	public ArrayList<Integer> findIntersection(ArrayList<Integer> set1, ArrayList<Integer> set2)
+	{
+		ArrayList<Integer> intersection = new ArrayList<Integer>();
+		
+		for (int element1 : set1)
+		{
+			for (int element2 : set2)
+			{
+				if (element1 == element2)
+					intersection.add(element1);
+			}
+		}
+		
+		return intersection;
 	}
 }
