5bfb033aaea51bc8afcd4932bd2b4de723a8919d
RyanMinneo <minneor@go.stockton.edu>
2023-01-30 23:28:28 -0500

    Started Working on findUnion method

--- ProbAndStat/src/Library/SetOperations.java
+++ ProbAndStat/src/Library/SetOperations.java
@@ -9,6 +9,10 @@ public class SetOperations {
 	
 	public ArrayList<Integer> findUnion(ArrayList<Integer> set1, ArrayList<Integer> set2)
 	{
+		ArrayList<Integer> union = new ArrayList<Integer>();
 		
+		union.addAll(set1);
+		
+		return union;
 	}
 }
