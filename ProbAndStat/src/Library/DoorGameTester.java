2310664abca65a6d9f1c1c5684be2cac013c46f7
RyanMinneo <minneor@go.stockton.edu>
2023-02-05 16:05:27 -0500

    Removed Lines of Code from DoorGameTester.
    Didn't need as many lines as I put, and the professor requires less than
    4 lines in the main method.

--- ProbAndStat/src/Library/DoorGameTester.java
+++ ProbAndStat/src/Library/DoorGameTester.java
@@ -1,17 +1,11 @@
 package Library;
 
-import java.util.ArrayList;
-
 public class DoorGameTester 
 {
 	
 	public static void main(String[] args)
 	{
 		DoorGame tester = new DoorGame();
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		
-		arr.addAll(tester.generateDoors());
-		System.out.println(arr.toString());
 		
 		System.out.println("The amount of times won is " + tester.playGame(10000, false));
 	}
